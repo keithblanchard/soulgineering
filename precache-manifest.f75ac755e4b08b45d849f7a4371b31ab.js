da4f0084ba8bbf9dc57e3d305474b6d24ebd65b4
Keith Edward <kblanchard111@gmail.com>
2020-11-14 21:37:41 -0600

    Using new deploy plugin.

---
+++ precache-manifest.f75ac755e4b08b45d849f7a4371b31ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbae63c2f5e81f6cdb0b8ca0725d9844",
+    "url": "/soulgineering-deploy/index.html"
+  },
+  {
+    "revision": "10232f228b0287f965be",
+    "url": "/soulgineering-deploy/static/css/main.404987ce.chunk.css"
+  },
+  {
+    "revision": "5cdb2325bfbffcfafb96",
+    "url": "/soulgineering-deploy/static/js/2.f541a603.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/soulgineering-deploy/static/js/2.f541a603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10232f228b0287f965be",
+    "url": "/soulgineering-deploy/static/js/main.9f608856.chunk.js"
+  },
+  {
+    "revision": "6c4b31ad1cc2ee2f5770",
+    "url": "/soulgineering-deploy/static/js/runtime-main.292b6ff5.js"
+  },
+  {
+    "revision": "0cf9c332644cd561b98092655e948410",
+    "url": "/soulgineering-deploy/static/media/logo-black-inner-gold-resized.0cf9c332.png"
+  },
+  {
+    "revision": "0895c79124a5a53fa110e2de9e6f73af",
+    "url": "/soulgineering-deploy/static/media/logo-no-text.0895c791.png"
+  },
+  {
+    "revision": "b5764a6708bbf0f91e985f8b3eb84037",
+    "url": "/soulgineering-deploy/static/media/logo-sound.b5764a67.wav"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/soulgineering-deploy/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "90ac50a691a68e2f94e5a16a1fc0d556",
+    "url": "/soulgineering-deploy/static/media/logo30_withLight.90ac50a6.png"
+  },
+  {
+    "revision": "4b1c303c6d0a51d594c92b09d4597d21",
+    "url": "/soulgineering-deploy/static/media/stars.4b1c303c.jpeg"
+  }
+]);
