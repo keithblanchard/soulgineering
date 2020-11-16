5e8c8a7ccdcfd85d90111dd612c1d42b5b211d40
Keith Edward <kblanchard111@gmail.com>
2020-11-16 00:56:44 -0600

    Update back to root.

---
+++ precache-manifest.1aff0d6ae78abe53c0508bacecafb5a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a701f35fec145f88bac7a3d55f9796b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be9ed27adb90ca82482a",
+    "url": "./static/css/main.3bfb79f5.chunk.css"
+  },
+  {
+    "revision": "088b2837b3e867f03783",
+    "url": "./static/js/2.78ac9e0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.78ac9e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9ed27adb90ca82482a",
+    "url": "./static/js/main.dfbba6b2.chunk.js"
+  },
+  {
+    "revision": "8810cdf06efd2fd7df7c",
+    "url": "./static/js/runtime-main.cf663cfc.js"
+  },
+  {
+    "revision": "6dc5bab84deea2a328685156d3016b19",
+    "url": "./static/media/VESPASIAN_TIMES.6dc5bab8.ttf"
+  },
+  {
+    "revision": "0cf9c332644cd561b98092655e948410",
+    "url": "./static/media/logo-black-inner-gold-resized.0cf9c332.png"
+  },
+  {
+    "revision": "0895c79124a5a53fa110e2de9e6f73af",
+    "url": "./static/media/logo-no-text.0895c791.png"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "./static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "90ac50a691a68e2f94e5a16a1fc0d556",
+    "url": "./static/media/logo30_withLight.90ac50a6.png"
+  },
+  {
+    "revision": "4b1c303c6d0a51d594c92b09d4597d21",
+    "url": "./static/media/stars.4b1c303c.jpeg"
+  }
+]);
